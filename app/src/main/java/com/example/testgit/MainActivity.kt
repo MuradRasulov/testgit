7cb7497198ae4dc817acf071d52e3e7041904c19
MuradRasulov <murad231190@mail.ru>
2022-06-22 16:09:56 +0500

    v4

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -2,58 +2,104 @@ package com.example.testgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private  lateinit var  question_text_view:TextView
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextView: TextView
 
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
     private val questionBank = listOf(
-        Question(R.string.question_australia   , true),
-        Question(R.string.question_oceans,
-            true),
-        Question(R.string.question_mideast,
-            false),
-        Question(R.string.question_africa,
-            false),
-        Question(R.string.question_americas
-            , true),
-        Question(R.string.question_asia,
-            true))
-
-
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa,false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true))
 
     private var currentIndex = 0
-    override fun onCreate(savedInstanceState:
-                          Bundle?) {
-        nextButton =findViewById(R.id.next_button)
-        question_text_view = findViewById(R.id.question_text_view)
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        trueButton = findViewById(R.id.true_button)
-        falseButton =findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener { view: View ->
+            setContentView(R.layout.activity_main)
+            val provider: ViewModelProvider = ViewModelProviders.of(this)
+            val quizViewModel = provider.get(QuizViewModel::class.java)
+            Log.d(TAG, "Got a QuizViewModel:$quizViewModel")
+            trueButton = findViewById(R.id.true_button)
+
+
+        var currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
+
+        Log.d(TAG,"onCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+
+        prevButton = findViewById(R.id.prev_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
             checkAnswer(true)
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
         }
-        falseButton.setOnClickListener { view: View ->
+        falseButton.setOnClickListener {
             checkAnswer(false)
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
         }
-            nextButton.setOnClickListener { currentIndex = (currentIndex + 1) % questionBank.size }
-            updateQuestion()
+            nextButton.setOnClickListener {
+                //quizViewModel.moveToNext()
+                updateQuestion()
+            }
+            prevButton.setOnClickListener {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }
         }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
     private fun updateQuestion() {
-        val questionTextResId =
-            questionBank[currentIndex].textResId
-        question_text_view.setText(questionTextResId)
+        val questionTextResId = quizViewModel.currentQuestionText
+        questionTextView.setText(questionTextResId)
     }
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         }
@@ -62,6 +108,12 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+    override fun onSaveInstanceState(savedInstanceState: Bundle)
+    {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG,"onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
 }
 
--- app/src/main/java/com/example/testgit/Question.kt
+++ app/src/main/java/com/example/testgit/Question.kt
@@ -2,5 +2,4 @@ package com.example.testgit
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId:
-                    Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
